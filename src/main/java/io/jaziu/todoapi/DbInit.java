9248e68b2eec2e387486ef1b828496c20243cdb4
Jan Nowacki <jasiujanow99@gmail.com>
2020-04-03 02:34:34 +0200

    09. Remote mySQL DB

--- src/main/java/io/jaziu/todoapi/DbInit.java
+++ src/main/java/io/jaziu/todoapi/DbInit.java
@@ -32,54 +32,54 @@ public class DbInit implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-//
-//
-//        roleRepository.save(new Role(RoleName.ROLE_USER));
-//        roleRepository.save(new Role(RoleName.ROLE_ADMIN));
-//
-//        User admin = new User("admin",encoder.encode("admin123"),"adminemail@gmail.com");
-//        User user = new User("user",encoder.encode("user123"),"useremail@gmail.com");
-//        Todo todo1 = new Todo("Finish this app",admin);
-//        Todo todo2 = new Todo("Create UI",admin);
-//        Todo todo3 = new Todo("Make coffe",user);
-//        Todo todo4 = new Todo("Find Job",user);
-//
-//        userRepository.save(admin);
-//        userRepository.save(user);
-//
-//        todoRepository.save(todo1);
-//        todoRepository.save(todo2);
-//        todoRepository.save(todo3);
-//        todoRepository.save(todo4);
-//
-//        Set<Todo> adminTodos= new HashSet<>();
-//        adminTodos.add(todo1);
-//        adminTodos.add(todo2);
-//        admin.setTodos(adminTodos);
-//
-//        Set<Todo> userTodos= new HashSet<>();
-//        userTodos.add(todo3);
-//        userTodos.add(todo4);
-//        user.setTodos(userTodos);
-//
-//        Set<Role> adminRoles = new HashSet<>();
-//        Set<Role> userRoles = new HashSet<>();
-//
-//        Role adminRole = roleRepository.findByName(RoleName.ROLE_ADMIN)
-//                .orElseThrow(() -> new RuntimeException("Fail! -> Cause: User Role not find."));
-//
-//        Role userRole = roleRepository.findByName(RoleName.ROLE_USER)
-//                .orElseThrow(() -> new RuntimeException("Fail! -> Cause: User Role not find."));
-//
-//        adminRoles.add(userRole);
-//        adminRoles.add(adminRole);
-//        userRoles.add(userRole);
-//
-//        admin.setRoles(adminRoles);
-//        userRepository.save(admin);
-//
-//        user.setRoles(userRoles);
-//        userRepository.save(user);
+
+
+        roleRepository.save(new Role(RoleName.ROLE_USER));
+        roleRepository.save(new Role(RoleName.ROLE_ADMIN));
+
+        User admin = new User("admin",encoder.encode("admin123"),"adminemail@gmail.com");
+        User user = new User("user",encoder.encode("user123"),"useremail@gmail.com");
+        Todo todo1 = new Todo("Finish this app",admin);
+        Todo todo2 = new Todo("Create UI",admin);
+        Todo todo3 = new Todo("Make coffe",user);
+        Todo todo4 = new Todo("Find Job",user);
+
+        userRepository.save(admin);
+        userRepository.save(user);
+
+        todoRepository.save(todo1);
+        todoRepository.save(todo2);
+        todoRepository.save(todo3);
+        todoRepository.save(todo4);
+
+        Set<Todo> adminTodos= new HashSet<>();
+        adminTodos.add(todo1);
+        adminTodos.add(todo2);
+        admin.setTodos(adminTodos);
+
+        Set<Todo> userTodos= new HashSet<>();
+        userTodos.add(todo3);
+        userTodos.add(todo4);
+        user.setTodos(userTodos);
+
+        Set<Role> adminRoles = new HashSet<>();
+        Set<Role> userRoles = new HashSet<>();
+
+        Role adminRole = roleRepository.findByName(RoleName.ROLE_ADMIN)
+                .orElseThrow(() -> new RuntimeException("Fail! -> Cause: User Role not find."));
+
+        Role userRole = roleRepository.findByName(RoleName.ROLE_USER)
+                .orElseThrow(() -> new RuntimeException("Fail! -> Cause: User Role not find."));
+
+        adminRoles.add(userRole);
+        adminRoles.add(adminRole);
+        userRoles.add(userRole);
+
+        admin.setRoles(adminRoles);
+        userRepository.save(admin);
+
+        user.setRoles(userRoles);
+        userRepository.save(user);
 
     }
 }
